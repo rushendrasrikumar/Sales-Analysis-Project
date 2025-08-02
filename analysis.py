6f7410d5650547ff23538243dd4f65742dffdfad
Rushendra Sri Kumar Sadhu <160743354+rushendrasrikumar@users.noreply.github.com>
2025-08-02 11:58:09 +0530

    Delete analysis.py

--- analysis.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-sales_data = pd.read_csv('C:/Users/mahen/OneDrive/Desktop/Rushi html/sales_force_project/train.csv')
-print(sales_data.head())
-sales_by_region = sales_data.groupby('Region')['Sales'].sum()
-print(sales_by_region)
